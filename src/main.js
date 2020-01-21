d3b4e703417387ad46b6f09d7b604f4ae312f498
ierosodin <ierosodin@gmail.com>
2020-01-21 10:51:12 +0800

    use bootstrap css library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue'
 import Vue2TouchEvents from 'vue2-touch-events'
+import {
+    LayoutPlugin, InputGroupPlugin, FormPlugin, FormGroupPlugin, FormInputPlugin,
+    FormCheckboxPlugin, FormSelectPlugin, ButtonPlugin, ButtonGroupPlugin, ModalPlugin,
+    PopoverPlugin, NavbarPlugin,
+} from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(LayoutPlugin);
+Vue.use(InputGroupPlugin);
+Vue.use(FormPlugin);
+Vue.use(FormGroupPlugin);
+Vue.use(FormInputPlugin);
+Vue.use(FormCheckboxPlugin);
+Vue.use(FormSelectPlugin);
+Vue.use(ButtonPlugin);
+Vue.use(ButtonGroupPlugin);
+Vue.use(ModalPlugin);
+Vue.use(PopoverPlugin);
+Vue.use(NavbarPlugin);
 Vue.use(Vue2TouchEvents)
 
 new Vue({
